9467201e2dd1521eee2bacc751ce683306b62ffb
mahero12 <132934622+mahero12@users.noreply.github.com>
2024-05-13 13:03:43 +0200

    Update mapa_interactivo.py

--- mapa_interactivo.py
+++ mapa_interactivo.py
@@ -1,8 +1,4 @@
 pip install folium
 import folium
 
-# Crea un mapa centrado en Estados Unidos
-mapa = folium.Map(location=[37.0902, -95.7129], zoom_start=4)
 
-# Guarda el mapa en un archivo HTML
-mapa.save('mapa_interactivo.html')
