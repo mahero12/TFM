7573aa6a1ab68586c653fbd6f54be543de607fcb
mahero12 <132934622+mahero12@users.noreply.github.com>
2024-05-14 10:09:45 +0200

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -1,16 +1,23 @@
 pip install folium
 import folium
 from folium.plugins import MarkerCluster
-import random
 import pandas as pd
+import requests
 
-file_path = "TFM_limpio.csv"
-num_rows = sum(1 for row in open(file_path, 'r', encoding='utf-8')) - 1
-sample_size = 20000
-skip_rows = sorted(random.sample(range(1, num_rows + 1), num_rows - sample_size)) 
+# URL de descarga directa del archivo CSV en Google Drive
+file_url = "https://drive.google.com/uc?export=download&id=1cIur-I2blbheoWd6fcLxt9WX-19HY9oG"
 
-df = pd.read_csv(file_path, skiprows=skip_rows)
+# Descargar el archivo CSV desde Google Drive
+response = requests.get(file_url)
 
+# Guardar el archivo CSV en el disco
+with open("US_Accidents.csv", "wb") as f:
+    f.write(response.content)
+
+# Leer el archivo CSV en un DataFrame de Pandas
+df = pd.read_csv("US_Accidents.csv")
+
+# Filtrar datos y crear el mapa
 df_loc = df.loc[(~df.Start_Lat.isna()) & (~df.Start_Lng.isna())]
 
 def create_map(df_loc, latitude, longitude, zoom, tiles='OpenStreetMap'):
@@ -20,40 +27,22 @@ def create_map(df_loc, latitude, longitude, zoom, tiles='OpenStreetMap'):
     world_map = folium.Map(location=[latitude, longitude], zoom_start=zoom, tiles=tiles)
     marker_cluster = MarkerCluster().add_to(world_map)
 
-    # Iterate over the DataFrame rows and add each marker to the cluster
+    # Iterar sobre las filas del DataFrame y añadir cada marcador al clúster
     for idx, row in df_loc.iterrows():
         folium.Marker(
             location=[row['Start_Lat'], row['Start_Lng']],
-            # You can add more attributes to your marker here, such as a popup
+            # Puedes añadir más atributos a tu marcador aquí, como un popup
             popup=f"Lat, Lng: {row['Start_Lat']}, {row['Start_Lng']}"
         ).add_to(marker_cluster)
 
     return world_map
 
-us_cities_coords = {
-    "New York": {"lat": 40.7128, "lon": -74.0060},
-    "Los Angeles": {"lat": 34.0522, "lon": -118.2437},
-    "Chicago": {"lat": 41.8781, "lon": -87.6298},
-    "Houston": {"lat": 29.7604, "lon": -95.3698},
-    "Phoenix": {"lat": 33.4484, "lon": -112.0740},
-    "Philadelphia": {"lat": 39.9526, "lon": -75.1652},
-    "San Antonio": {"lat": 29.4241, "lon": -98.4936},
-    "San Diego": {"lat": 32.7157, "lon": -117.1611},
-    "Dallas": {"lat": 32.7767, "lon": -96.7970},
-    "San Jose": {"lat": 37.3382, "lon": -121.8863},
-    "Austin": {"lat": 30.2672, "lon": -97.7431},
-    "Jacksonville": {"lat": 30.3322, "lon": -81.6557},
-    "Fort Worth": {"lat": 32.7555, "lon": -97.3308},
-    "Columbus": {"lat": 39.9612, "lon": -82.9988},
-    "San Francisco": {"lat": 37.7749, "lon": -122.4194},
-    "Charlotte": {"lat": 35.2271, "lon": -80.8431},
-    "Indianapolis": {"lat": 39.7684, "lon": -86.1581},
-    "Seattle": {"lat": 47.6062, "lon": -122.3321},
-    "Denver": {"lat": 39.7392, "lon": -104.9903},
-    "Washington": {"lat": 38.9072, "lon": -77.0369}
-}
+# Coordenadas para centrar el mapa
+latitude = 39.50
+longitude = -98.35
 
-map_us = create_map(df_loc, 39.50, -98.35, 4)
+# Crear el mapa
+map_us = create_map(df_loc, latitude, longitude, 4)
 
 # Obtener el HTML del mapa generado
 index_html = map_us.get_root().render()
